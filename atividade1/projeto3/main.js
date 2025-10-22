8037314d52ba81336866751779bfaf7e1790f15a
Lenildo-Damasceno <lenildo58479636@edum.rn.senac.br>
2025-10-21 23:18:55 -0300

    ✨ atualização das atividades

---
+++ atividade1/projeto3/main.js
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from 'electron';
+
+function criarJanela() {
+  const janela = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  janela.loadFile('index.html');
+}
+
+app.whenReady()
+  .then(() => {
+    criarJanela();
+    console.log('Executando Electron - Projeto 3');
+  })
+  .catch((erro) => {
+    console.error(erro);
+  });
