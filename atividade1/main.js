3f2b85767e4b2d986318fdc0196224c36bf2cf72
Lenildo-Damasceno <lenildo58479636@edum.rn.senac.br>
2025-10-23 20:46:25 -0300

    ✨ Atualização

--- atividade1/main.js
+++ atividade1/main.js
@@ -1,19 +1,28 @@
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-function criarJanela() {
+// ...existing code...
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function criarJanela() { // Função para criar a janela principal
   const janela = new BrowserWindow({
     width: 800,
     height: 600,
+    resizable: false, //tamanho fixo
+    fullscreen: false,  //tela cheia desativada
+    icon: __dirname + '/icon.png' // Caminho para o ícone da aplicação
   });
 
-  janela.loadFile('resposta1.html');
+  janela.loadFile('resposta1.html'); // Carrega o arquivo HTML na janela ou uma pagina 
 }
 
-app.whenReady()
+app.whenReady() // Quando o aplicativo estiver pronto
   .then(() => {
     criarJanela();
-    console.log('Executando Electron - Projeto 1');
+    console.log('Executando Electron ');
   })
-  .catch((erro) => {
+  .catch((erro) => {  // Tratamento de erros
     console.error(erro);
   });
